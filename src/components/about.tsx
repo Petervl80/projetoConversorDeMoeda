b5b403f1c58fa33a7b0172ad340638180df140f4
Pedro Vinícius <133929247+Petervl80@users.noreply.github.com>
2023-10-04 14:17:26 -0300

    Update about.tsx ainda falta

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,12 +6,7 @@ export function About() {
       </h2>
 
       <p className="text-muted-foreground first-letter:font-bold first-letter:text-2xl">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur earum repellendus iusto cupiditate quod in, laborum illum ipsam quaerat enim. Aut, illo. Iste itaque natus fugit officiis, cumque similique voluptas.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere ad officiis, voluptate consequuntur molestias debitis eum in doloribus excepturi ducimus quas nam dolor ab atque quidem. Dolorem blanditiis atque quaerat!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, magni quos eum at asperiores tenetur saepe, aut dignissimos assumenda similique voluptatem excepturi cumque rerum quam facere. Repellendus sit voluptatum vitae!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus veniam aperiam alias numquam officiis, pariatur distinctio eaque hic sunt, corrupti quaerat obcaecati necessitatibus natus ea architecto aliquam nobis impedit laborum!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptate temporibus, consequatur dolor atque ea voluptatum libero dolorem eligendi, illo sapiente commodi a saepe eveniet sequi delectus repudiandae fuga eius.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos nulla, voluptatibus, deserunt accusantium, doloribus voluptatum eius laudantium fugit corrupti facilis praesentium! Quisquam repellat repellendus tenetur sapiente incidunt ut nobis.
+        Este sistema envolve um conversor de moedas em tempo real.
       </p>
     </>
   )
